2fd1570af3752ae100ebde050044de5a120ea55c
KustelR <dmitriysstudios2@gmail.com>
2025-05-04 14:24:21 +0400

    created mock sync functionality

--- src/components/Device/Device.tsx
+++ src/components/Device/Device.tsx
@@ -4,13 +4,39 @@ import { DeviceProps } from ".";
 import styles from "./styles";
 import DeviceStatus from "./DeviceStatus";
 import DeviceActions from "./DeviceActions";
-import DeviceHeader from "./DeviceHeader";
+import DeviceHeader, { SyncStatus } from "./DeviceHeader";
+import { useState } from "react";
 
 export default function Device(props: DeviceProps) {
   const { data } = props;
+
+  const [syncState, setSyncState] = useState<SyncStatus | undefined>(undefined);
+  const [isSyncLocked, setIsSyncLocked] = useState<boolean>(false);
+
   return (
     <View style={styles.device}>
-      <DeviceHeader name={data.name} />
+      <DeviceHeader
+        name={data.name}
+        syncStatus={syncState}
+        isSyncAnimated={isSyncLocked}
+        onSyncTouch={() => {
+          setIsSyncLocked(true);
+          if (isSyncLocked) {
+            return;
+          }
+          if (Math.random() > 0.5) {
+            setTimeout(() => {
+              setSyncState("fail");
+              setIsSyncLocked(false);
+            }, 1000);
+          } else {
+            setTimeout(() => {
+              setSyncState("success");
+              setIsSyncLocked(false);
+            }, 1000);
+          }
+        }}
+      />
       <DeviceStatus data={data.status} />
       <DeviceActions data={data} />
     </View>
--- src/components/Device/DeviceHeader.tsx
+++ src/components/Device/DeviceHeader.tsx
@@ -1,22 +1,33 @@
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  useAnimatedValue,
+  Animated,
+  ViewStyle,
+} from "react-native";
 import styles from "./styles";
 import { Image } from "expo-image";
-import { useEffect, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
+import RotatingView from "@/src/components/RotatingView";
 
 export interface DeviceHeaderProps {
   name: string;
   syncStatus?: SyncStatus;
+  isSyncAnimated?: boolean;
   onSyncTouch?: () => void;
   onEditTouch?: () => void;
 }
 
 export default function DeviceHeader(props: DeviceHeaderProps) {
-  const { name, syncStatus, onSyncTouch, onEditTouch } = props;
+  const { name, syncStatus, onSyncTouch, onEditTouch, isSyncAnimated } = props;
   return (
     <View style={styles.deviceHeader}>
       <Text style={styles.deviceHeaderText}>{name}</Text>
       <DeviceControls
         syncStatus={syncStatus}
+        isSyncAnimated={isSyncAnimated}
         onEditTouch={onEditTouch}
         onSyncTouch={onSyncTouch}
       />
@@ -28,15 +39,15 @@ export type SyncStatus = "success" | "fail";
 
 interface DeviceControlsProps {
   syncStatus?: SyncStatus;
+  isSyncAnimated?: boolean;
   onSyncTouch?: () => void;
   onEditTouch?: () => void;
 }
-
 function DeviceControls(props: DeviceControlsProps) {
-  const { syncStatus, onSyncTouch, onEditTouch } = props;
+  const { syncStatus, onSyncTouch, onEditTouch, isSyncAnimated } = props;
   const syncUnknown = require(`@/assets/images/sync-unknown.svg`);
-  const syncSuccess = require(`@/assets/images/sync-unknown.svg`);
-  const syncFail = require(`@/assets/images/sync-unknown.svg`);
+  const syncSuccess = require(`@/assets/images/sync-success.svg`);
+  const syncFail = require(`@/assets/images/sync-fail.svg`);
 
   const [syncIcon, setSyncIcon] = useState<any>(null);
 
@@ -56,9 +67,13 @@ function DeviceControls(props: DeviceControlsProps) {
   return (
     <View style={localStyles.controls}>
       <TouchableHighlight>
-        <View style={localStyles.controlIcon} onTouchStart={onSyncTouch}>
+        <RotatingView
+          style={localStyles.controlIcon}
+          isAnimated={isSyncAnimated}
+          onTouchStart={onSyncTouch}
+        >
           <Image style={{ width: "90%", height: "90%" }} source={syncIcon} />
-        </View>
+        </RotatingView>
       </TouchableHighlight>
       <TouchableHighlight>
         <View style={localStyles.controlIcon} onTouchStart={onEditTouch}>
