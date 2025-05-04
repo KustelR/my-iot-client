d3587b85f257e7ca79546f60beef1f1116b98b32
KustelR <dmitriysstudios2@gmail.com>
2025-05-04 12:06:56 +0400

    type for gsm devices

--- src/components/Device/index.ts
+++ src/components/Device/index.ts
@@ -5,6 +5,12 @@ export interface DeviceData {
   name: string;
   status: DeviceStatusData;
   actions: DeviceActionData[];
+  deviceType?: DeviceType;
+}
+
+export interface DeviceDataGSM extends DeviceData {
+  deviceType: "gsm";
+  phoneNumber: string;
 }
 
 export interface DeviceStatusData {
@@ -20,3 +26,11 @@ export interface DeviceActionData {
 export type DeviceProps = {
   data: DeviceData;
 };
+
+export type DeviceType = "gsm";
+
+export function isDeviceType(data: string): data is DeviceType {
+  const deviceTypes = ["gsm"];
+  if (deviceTypes.includes(data)) return true;
+  return false;
+}
